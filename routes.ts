12c70606f6da83047fa71e74a933eb2fa9fb8f98
Felipe <nascimentofelipe565@gmail.com>
2021-02-26 19:48:31 -0300

    5th day done

--- routes.ts
+++ routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { AnswerController } from './src/controllers/AnswerController';
+import { NpsController } from './src/controllers/NpsController';
 import { SendMailController } from './src/controllers/SendMailController';
 import { SurveysController } from './src/controllers/SurveysController';
 import { UserController } from './src/controllers/UserController';
@@ -7,6 +9,8 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -14,5 +18,9 @@ router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
