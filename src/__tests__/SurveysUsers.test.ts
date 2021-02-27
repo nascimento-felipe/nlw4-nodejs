0a1860c587017da812be78a0c0d26ae503b493e6
Felipe <nascimentofelipe565@gmail.com>
2021-02-26 22:59:34 -0300

    3rd test done, but not working

---
+++ src/__tests__/SurveysUsers.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import { getConnection } from 'typeorm';
+import { app } from '../app';
+import createConnection from '../database';
+
+describe("SurveyUser", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        const connection = getConnection();
+
+        await connection.dropDatabase();
+        await connection.close();
+    })
+
+    it("Should create a email, linking a survey and a user in the database", async () => {
+        await request(app).post("/surveys")
+            .send({
+                title: "teste",
+                description: "teste 2"
+            });
+
+        const survey = await request(app).get("surveys");
+
+        await request(app).post("/users")
+            .send({
+                name: "Usuario",
+                email: "usuario@email.com"
+            });
+
+        const response = await request(app).post("/sendMail")
+            .send({
+                email: "usuario@email.com",
+                survey_id: survey,
+            })
+
+        expect(response.status).toBe(200);
+    })
+})
--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -4,22 +4,26 @@ import { app } from '../app';
 
 import createConnection from '../database'
 
+// é o que acontece no /Surveys.test.ts
+
+// Começo do bloco de testes
 describe("User", () => {
-    beforeAll(async () => {
-        const connection = await createConnection();
+    beforeAll(async () => { // Isso aqui é pra começar os testes, antes de tudo é necessário criar um banco de dados
+        const connection = await createConnection(); // pra poder fazer os testes sem danificar o BD da aplicação.
         await connection.runMigrations();
     });
 
-    afterAll(async () => {
+    afterAll(async () => { // Aqui é ele falando que depois de tudo, é  necessário dropar a DB de testes criada.
         const connection = getConnection();
 
         await connection.dropDatabase();
         await connection.close();
     });
 
-    it("Should be able to create a new user", async () => {
-        const response = await request(app).post("/users")
-            .send({
+    // Primeiro teste
+    it("Should be able to create a new user", async () => { // Esse teste é para criar um novo usuário. Pra isso, 
+        const response = await request(app).post("/users") // a rota "/users" é acessada no método POST e espera-se
+            .send({                                        // que a o status da resposta seja 201, de 'criado'.
                 email: "user@example.com",
                 name: "User Example"
             });
@@ -27,10 +31,11 @@ describe("User", () => {
         expect(response.status).toBe(201);
     });
 
+    // Segundo teste
     it("Should not be able to create a user with an email which a already exists", async () => {
-        const response = await request(app).post("/users")
-            .send({
-                email: "user@example.com",
+        const response = await request(app).post("/users") // Já esse segundo teste é pra não deixar criar um usuário
+            .send({                                        // com um email já usado. Pra isso, ele cria um usuário igual
+                email: "user@example.com",                 // o do teste 1, e espera-se que o status da resposta seja 400.
                 name: "User Example"
             });
 
