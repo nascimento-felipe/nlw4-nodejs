f147bcb4274b917edb1125d972352a1e6bd8fc2d
Felipe <nascimentofelipe565@gmail.com>
2021-03-01 21:46:52 -0300

    comments in database folder added

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
+    // pega as opções padrões da conexão com o BD
     const defaultOptions = await getConnectionOptions();
+
+    // se a variável de ambiente 'NODE_ENV' tiver o valor "test", então o banco de dados usado vai ser o de testes,
+    // se não, vai ser o banco de dados passado dentro das opções padronizadas.
     return createConnection(
         Object.assign(defaultOptions, {
             database: process.env.NODE_ENV === 'test'
