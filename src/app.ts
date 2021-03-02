70b511a0dd721bdfb13da8500845e401b33b74c0
Felipe <nascimentofelipe565@gmail.com>
2021-03-02 16:25:26 -0300

    comments added

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,26 @@ import createConnection from "./database";
 import { router } from '../routes';
 import { AppError } from './errors/AppError';
 
+// aqui é criada uma conexão com o banco de dados
 createConnection();
+// constante app recebe o express 
 const app = express();
 
+// app pode usar o json e o router, pra poder pegar as rotas
 app.use(express.json());
 app.use(router);
 
+// se algum erro por capturado, ele é retornado
 app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+    // se for um erro que nós colocamos como regra (tipo aqueles de !user ou surveyAlreadyExist), ele manda a mensagem
+    // colocada lá e também o status code
     if (err instanceof AppError) {
         return response.status(err.statusCode).json({
             message: err.message
         });
     };
 
+    // se não for, é um erro interno do server, então ele envia a mensagem do erro e o status de 500
     return response.status(500).json({
         status: "Error",
         message: `Internal server error: ${err.message}`
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import { app } from "./app";
 
+// aqui ele roda o servidor quando o comando "yarn dev" é digitado.
+// foi dividido assim pra poder acessar dentro dos testes, sem ligar o servidor
+// daí dá pra usar o servidor próprio da biblioteca de testes. (a supertest) 
 app.listen(3333, () => console.log('Server is running'));
